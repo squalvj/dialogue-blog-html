ac0081074e887b64ca040bc74ce67179f3090ef9
Table Six <tablesix@ZARATHUSTRA.local>
2018-01-22 18:10:52 +0700

    finish single

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,129 +6,24 @@ $(document).ready(function() {
 });
 
 function init(){
- 	setCarouselHome();
- 	restartImgOwl();
- 	setGallerySingle();
+	//setCarouselSingle();
+	afterglow.getPlayer('myvideo');
 }
 
-function setCarouselHome(){
-	var nav = ["<img src='assets/img/left-arrow.png'>", "<img src='assets/img/right-arrow.png'>"]
-    var navC = '#navigasi-owl';
- 	$('#carousel-home').owlCarousel({
-	    loop:true,
-	    margin:10,
-	    nav:true,
-	    items:1,
-	 	autoHeight:true,
-		navContainer: navC,
-		navText: nav,
-	})
-}
-
-function setGallerySingle(){
-	var w = $(window).width();
-	var nav = ["<button><i class='fa fa-caret-left' aria-hidden='true'></i></button>", "<button><i class='fa fa-caret-right' aria-hidden='true'></i></button>"]
-    var navC = '.navigasi-gallery';
-	$('.owl-gallery').owlCarousel({
+function setCarouselSingle(){
+	var nav = ["<button><img src='assets/img/left-arrow.png'></button>", "<button><img src='assets/img/right-arrow.png'></button>"]
+    var navC = '.navigasi-carousel';
+	$('.carousel-single').owlCarousel({
 	    margin:10,
 	    autoHeight:true,
 	    nav:true,
-	    items:5,
+	    items:1,
 	    navContainer: navC,
 		navText: nav,
-		dots: w < 768 ? true : false,
-		responsive:{
-	        0:{
-	            items:1,
-	            nav:false
-	        },
-	        768:{
-	            items:3,
-	        },
-	        1000:{
-	            items:5,
-	        }
-    	}
+		dots: true
 	})
 }
 
-function restartImgOwl(){
-	var img = $(".img-slider")
-	if (img.closest('.owl-height').height() === 1) { img.closest('.owl-carousel').trigger('refresh.owl.carousel'); }
-}
-
-
-$(".gallery-item").click(function(event) {
-	$(".owl-gallery").find(".gallery-item").removeClass('active')
-	$(this).addClass('active')
-	var src = $(this).find('img').attr('src')
-	var img = $("#thumb")
-	var feather = $(".preview-gallery > a").attr('href', src);
-	img.attr('src', '...');
-	img.hide();
-	img.attr('src', src);
-	$(".gallery-loader").css('display','flex');
-	img.on('load', function(){
-		$(".gallery-loader").css('display','none');
-		img.show();
-	})
-	
-});
-
-$(".hamburger").click(function(event) {
-	$(".nav-mobile").addClass('active')
-	$("body").addClass('no-scroll')
-});
-
-$(".close-nav").click(function(event) {
-	$(".nav-mobile").removeClass('active')
-	$("body").removeClass('no-scroll')
-});
-
-$(".dropdown").click(function(event) {
-	$(this).toggleClass('active');
-})
-
-$(".search-btn .util-img").click(function(event) {
-	var parent = $(this).parent('.search-btn')
-	if (parent.find('.search-bar').hasClass('active')){
-		parent.find('.search-bar').removeClass('active')
-	}else{
-		parent.find('.search-bar').addClass('active');
-		$("#search-form input").focus();
-	}
-	if (parent.find('input').val() == ''){
-		return;
-	}else{
-		$("#search-form").submit();
-		console.log("SUBMIT")
-	}
-});
-
-$(".search-btn input").click(function(e) {
-    e.stopPropagation();
-});
-
-$(".submit-item").click(function(event) {
-	$(".modal").addClass('active')
-	$("body").addClass('no-scroll')
-});
-
-$(".modal").click(function(event) {
-	if ($(this).hasClass('active')){
-		$(this).removeClass('active')
-		$("body").removeClass('no-scroll')
-	}
-});
-
-$(".btn-shopagain").click(function(event) {
-	$(".modal").removeClass('active')
-	$("body").removeClass('no-scroll')
-});
-
-$(".modal *").click(function(e) {
-    e.stopPropagation();
-});
 
 $(document).click(function(e) {
     var target = e.target;
@@ -137,3 +32,7 @@ $(document).click(function(e) {
         $("#search-form input").val("")
     }
 });
+
+$(window).on('load',function(){
+	setCarouselSingle();
+})
